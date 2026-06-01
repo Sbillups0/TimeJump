5d103fc7d0bc0e4a8d3e6d1658401815d95401a1
Sbillups0 <sbillups0@me.com>
2026-05-31 22:25:34 -0700

    added aim assist and fixed some slight issues with enemy

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
@@ -6,17 +7,38 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] private bool invincible = false;
     [SerializeField] private int maxHealth = 1;
 
+    [Header("Death")]
+    [SerializeField] private float destroyDelay = 0.7f;
+    [SerializeField] private string deathTrigger = "Die";
+    [SerializeField] private bool disableCollidersOnDeath = true;
+    [SerializeField] private bool stopMovementOnDeath = true;
+
     private int currentHealth;
+    private bool isDead;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private Collider2D[] colliders;
+    private EnemyShooterAI enemyShooterAI;
 
     public bool Invincible => invincible;
+    public bool IsDead => isDead;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        colliders = GetComponentsInChildren<Collider2D>();
+        enemyShooterAI = GetComponent<EnemyShooterAI>();
     }
 
     public void TakeDamage(int damage)
     {
+        if (isDead)
+            return;
+
         if (invincible)
         {
             Debug.Log(name + " is invincible.");
@@ -36,7 +58,47 @@ public class EnemyHealth : MonoBehaviour
 
     private void Die()
     {
+        if (isDead)
+            return;
+
+        isDead = true;
+
         Debug.Log(name + " died.");
+
+        if (enemyShooterAI != null)
+        {
+            enemyShooterAI.enabled = false;
+        }
+
+        if (stopMovementOnDeath && rb != null)
+        {
+            rb.linearVelocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+            rb.bodyType = RigidbodyType2D.Kinematic;
+        }
+
+        if (disableCollidersOnDeath && colliders != null)
+        {
+            foreach (Collider2D col in colliders)
+            {
+                if (col != null)
+                {
+                    col.enabled = false;
+                }
+            }
+        }
+
+        if (animator != null)
+        {
+            animator.SetTrigger(deathTrigger);
+        }
+
+        StartCoroutine(DestroyAfterDelay());
+    }
+
+    private IEnumerator DestroyAfterDelay()
+    {
+        yield return new WaitForSeconds(destroyDelay);
         Destroy(gameObject);
     }
 }
