cef79b85a7ba96432f50f3313dc2a8035a66d32d
hchencp <henrychencp@gmail.com>
2026-06-03 15:40:21 -0700

    Tutorial Sign and Respawn Enemies added

--- Assets/Scripts/IP-Cat Script/IPCatHealth.cs
+++ Assets/Scripts/IP-Cat Script/IPCatHealth.cs
@@ -6,10 +6,20 @@ public class IPCatHealth : MonoBehaviour
 
     public void Kill()
     {
+        // Respawn all enemies
+        if (EnemyManager.Instance != null)
+            EnemyManager.Instance.RespawnAllEnemies();
+
+        // Reset player position and velocity
         transform.position = respawnPoint.position;
 
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         if (rb != null)
             rb.linearVelocity = Vector2.zero;
+
+        // Reset player state
+        Player player = GetComponent<Player>();
+        if (player != null)
+            player.ResetState();
     }
 }
